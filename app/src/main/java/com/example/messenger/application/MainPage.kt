f16e31c78b7256e915dae652afad68b220c61d13
maxim <satanov201354@gmail.com>
2023-09-03 20:29:57 +0600

    added image saver

--- app/src/main/java/com/example/messenger/application/MainPage.kt
+++ app/src/main/java/com/example/messenger/application/MainPage.kt
@@ -2,11 +2,14 @@
 
 package com.example.messenger.application
 
-
+import android.content.Context
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,14 +22,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
@@ -37,23 +36,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.NavigationDrawerItemColors
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.contentColorFor
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -63,24 +57,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.net.toUri
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.messenger.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.io.File
 
 @Composable
 fun MainPage(
@@ -190,7 +183,7 @@ fun MainPage(
                             ),
                             title = {
                                 Text(
-                                    text = "Aues Portal",
+                                    text = "aues scraper",
                                     maxLines = 1,
                                     overflow = TextOverflow.Ellipsis,
                                     color = Color.White,
@@ -235,7 +228,7 @@ fun MainPage(
                             verticalArrangement = Arrangement.Center,
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            CreateImageProfile()
+                            CreateImageProfile(username)
                             Spacer(Modifier.height(40.dp))
                             CreateInfo(username, name, group)
                             Spacer(Modifier.height(40.dp))
@@ -285,25 +278,88 @@ data class MenuItem(
 )
 
 @Composable
-private fun CreateImageProfile() {
-    Surface(
-        modifier = Modifier
-            .size(200.dp)
-            .padding(5.dp),
-        shape = CircleShape,
-        border = BorderStroke(0.5.dp, Color.LightGray),
-        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
-        shadowElevation = 4.dp
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_pic),
-            contentDescription = "profile image",
-            modifier = Modifier.size(200.dp),
-            contentScale = ContentScale.Crop
-        )
+private fun CreateImageProfile(username: String) {
 
+    val context = LocalContext.current
+
+    val imageUri = rememberSaveable() {
+        mutableStateOf("")
+    }
+    val painter = rememberAsyncImagePainter(
+        if (imageUri.value.isEmpty()) {
+            val fileName = "image_${username}.jpg"
+            val file = File(context.filesDir, fileName)
+            if (file.exists()) {
+                file.toUri()
+            } else {
+                R.drawable.ic_user
+            }
+        } else {
+            imageUri.value
+        }
+    )
+
+    val isImageSelected = imageUri.value.isNotEmpty()
+
+    var isSaveButtonClicked by remember { mutableStateOf(false) }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) {
+            uri: Uri? ->
+        uri?.let { imageUri.value = it.toString() }
+        isSaveButtonClicked = false
+    }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            modifier = Modifier
+                .size(200.dp)
+                .padding(5.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+            shadowElevation = 4.dp
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { launcher.launch("image/*") },
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if (isImageSelected && !isSaveButtonClicked) {
+            Button(
+                onClick = {
+                    isSaveButtonClicked = true
+                    saveImageToInternalStorage(context, Uri.parse(imageUri.value), username)
+                },
+                colors = ButtonDefaults.buttonColors(Color(0xFF7289DA))
+            ) {
+                Text(text = "Сохранить изображение")
+            }
+        }
     }
 }
+
+fun saveImageToInternalStorage(context: Context, uri: Uri, username: String) {
+    val fileName = "image_${username}.jpg"
+    val inputStream = context.contentResolver.openInputStream(uri)
+    val outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE)
+    inputStream?.use { input ->
+        outputStream.use { output ->
+            input.copyTo(output)
+        }
+    }
+}
+
+
 
 @Composable
 private fun CreateImageDrawer() {
@@ -312,6 +368,7 @@ private fun CreateImageDrawer() {
             .size(200.dp)
             .padding(5.dp),
         shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
         color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
         shadowElevation = 4.dp
     ) {
